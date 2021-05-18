312b9702bcb33dfefee9ce6d1e7f123eb65e6adc
tuananhqro <tqtuananhqb@gmail.com>
2021-05-18 22:39:25 +0700

    Flight App init

---
+++ src/test/java/faber/app/flightapp/FlightAppApplicationTests.java
@@ -0,0 +1,13 @@
+package faber.app.flightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
